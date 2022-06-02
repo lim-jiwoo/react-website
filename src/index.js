c900bea7d3aa1ceddd3008dc3b5a569ca87eb532
lim-jiwoo <il04131@naver.com>
2022-06-02 20:40:35 +0900

    docs: add css files for App.js and src/index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 /* index.js 파일에서만 필요하다. 
 우리 React app을 index.html에 hook할 수 있게 만들어줄 것이다. */
 import App from './App';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
