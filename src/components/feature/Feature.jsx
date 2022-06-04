2b8db30d0c5a00275f7fb6ebce06b9e905336221
lim-jiwoo <il04131@naver.com>
2022-06-04 23:30:58 +0900

    feat: make basic structure of Feature component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = () => {
+const Feature = ({ title, text }) => { {/* props 로 받음 */}
   return (
-    <div>Feature</div>
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div /> {/* 여백을 만들기 위해 */}
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container__feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
   )
 }
 
